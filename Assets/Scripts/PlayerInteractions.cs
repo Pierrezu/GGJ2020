4863d600ac1f89b07d0e37e6acaf6b45dde47d98
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2020-02-01 01:39:09 +0100

    Player controller + wip interactions

---
+++ Assets/Scripts/PlayerInteractions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    private PlayerController playercontroller;
+
+    private void Start()
+    {
+        playercontroller = GetComponent<PlayerController>();
+    }
+    void Update()
+    {
+        if(playercontroller.playerNumber == 1)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                Debug.Log(playercontroller.playerNumber + "st player interact");
+            }
+        }
+        if (playercontroller.playerNumber == 2)
+        {
+            if (Input.GetButtonDown("Interact2"))
+            {
+                Debug.Log(playercontroller.playerNumber + "nd player interact");
+            }
+        }
+    }
+    private void Interact()
+    {
+
+    }
+}
