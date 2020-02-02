e0cf2505764fcf424a63acc1f75aaa5566fbf8fc
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2020-02-02 13:25:52 +0100

    Test correct bug pile et set 60 seconds before destroy charcoal

--- Assets/Scripts/FabricsBehaviour.cs
+++ Assets/Scripts/FabricsBehaviour.cs
@@ -10,7 +10,7 @@ public class FabricsBehaviour : MonoBehaviour
     public Transform spawningPoint;
     public Vector3 direction;
     public float speed;
-
+    public float timeBeforeDestroy = 60f;
     private void Start()
     {
         StartCreating();
@@ -27,7 +27,7 @@ public class FabricsBehaviour : MonoBehaviour
             clone.AddComponent<Rigidbody>();
             clone.GetComponent<Rigidbody>().useGravity = false;
             clone.GetComponent<Rigidbody>().velocity = direction.normalized * speed;
-            Destroy(clone, 10f);
+            Destroy(clone, timeBeforeDestroy);
         }
 
     }
--- Assets/Scripts/ObjectsAttributes.cs
+++ Assets/Scripts/ObjectsAttributes.cs
@@ -7,4 +7,16 @@ public class ObjectsAttributes : MonoBehaviour
     public bool isCarryied = false;
     public bool isCharcoal;
     public bool isABattery;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isABattery)
+        {
+            if (GetComponent<Rigidbody>() != null)
+            {
+                GetComponent<Rigidbody>().useGravity = false;
+            }
+        }
+
+    }
 }
