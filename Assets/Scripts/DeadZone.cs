3cf6800208ab3385508b2a03fe39df735fe40815
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2020-02-02 12:33:44 +0100

    DeadZone script

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Carryable") && other.GetComponent<ObjectsAttributes>() != null)
+        {
+            if (other.GetComponent<ObjectsAttributes>().isABattery== false)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
